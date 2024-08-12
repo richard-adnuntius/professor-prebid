c5171fe6d5337b174f1d89b9aad01cc00c4b5e34
Richard Masterton <richard@adnuntius.com>
2024-08-12 11:30:07 +1000

    Show blank rectangles in overlay

--- src/pages/App/components/AdOverlayComponent.tsx
+++ src/pages/App/components/AdOverlayComponent.tsx
@@ -53,24 +53,13 @@ const AdOverlayComponent = ({
   }, [gridRef.current?.offsetHeight, boxRef.current?.offsetHeight, truncate]);
   return (
     <ThemeProvider theme={theme}>
-      <PopOverComponent
-        elementId={elementId}
-        winningCPM={winningCPM}
-        winningBidder={winningBidder}
-        currency={currency}
-        timeToRespond={timeToRespond}
-        closePortal={closePortal}
-        anchorEl={anchorEl}
-        setAnchorEl={setAnchorEl}
-        pbjsNameSpace={pbjsNameSpace}
-      />
       <CacheProvider value={cache}>
         <Box
           ref={boxRef}
           sx={{
             height: expanded ? '100%' : 'auto',
             width: '100%',
-            opacity: 0.9,
+            opacity: 1,
             backgroundColor: 'primary.light',
             color: 'text.primary',
             padding: 0.5,
@@ -83,91 +72,6 @@ const AdOverlayComponent = ({
           }}
         >
           <Grid container justifyContent="flex-start" alignItems="flex-start" ref={gridRef} rowSpacing={0.2}>
-            <Grid container item justifyContent="space-between" alignItems="flex-start">
-              <Grid item xs={7} lg={3.5} xl={1.5}>
-                <Paper elevation={1} sx={{ p: 0.25 }}>
-                  <Typography
-                    variant="h4"
-                    sx={{
-                      wordWrap: 'break-word',
-                      textAlign: 'left',
-                    }}
-                  >
-                    {elementId}
-                  </Typography>
-                </Paper>
-              </Grid>
-              <Grid
-                item
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  justifyContent: 'flex-end',
-                  alignItems: 'flex-end',
-                  color: 'text.secondary',
-                }}
-                xs={4}
-              >
-                <IconButton sx={{ p: 0 }} onClick={() => setExpanded(!expanded)}>
-                  {expanded && <MinimizeIcon sx={{ fontSize: 14 }} />}
-                  {!expanded && <MaximizeIcon sx={{ fontSize: 14 }} />}
-                </IconButton>
-
-                <IconButton sx={{ p: 0 }} onClick={openInPopOver}>
-                  <OpenInFullIcon sx={{ fontSize: 14 }} />
-                </IconButton>
-
-                {window.parent.googletag && typeof window.parent.googletag?.pubads === 'function' && (
-                  <IconButton
-                    sx={{ p: 0 }}
-                    onClick={() => {
-                      window.parent.googletag.pubads().refresh([slot]);
-                    }}
-                  >
-                    <Refresh sx={{ fontSize: 14 }} />
-                  </IconButton>
-                )}
-
-                <IconButton sx={{ p: 0 }} onClick={closePortal}>
-                  <Close sx={{ fontSize: 14 }} />
-                </IconButton>
-              </Grid>
-            </Grid>
-            {expanded && (currency || winningBidder || winningCPM || timeToRespond || elementId) && (
-              <Grid container item xs={12} spacing={0.5}>
-                {winningCPM && (
-                  <Grid item>
-                    <Paper elevation={1} sx={{ p: 0.5 }}>
-                      <Typography>
-                        <strong>CPM: </strong>
-                        {winningCPM} {currency}
-                      </Typography>
-                    </Paper>
-                  </Grid>
-                )}
-                {winningBidder && (
-                  <Grid item>
-                    <Paper elevation={1} sx={{ p: 0.5 }}>
-                      <Typography>
-                        <strong>Bidder: </strong>
-                        {winningBidder}
-                      </Typography>
-                    </Paper>
-                  </Grid>
-                )}
-                {timeToRespond && (
-                  <Grid item>
-                    <Paper elevation={1} sx={{ p: 0.5 }}>
-                      <Typography>
-                        <strong>TTR: </strong>
-                        {timeToRespond}ms
-                      </Typography>
-                    </Paper>
-                  </Grid>
-                )}
-                {elementId && <GamDetailsComponent elementId={elementId} inPopOver={false} truncate={truncate} />}
-              </Grid>
-            )}
           </Grid>
         </Box>
       </CacheProvider>
